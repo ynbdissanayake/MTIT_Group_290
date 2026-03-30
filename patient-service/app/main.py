35d9b4eb4525714e648ef64424f7ad7169825fa8
YasiruNB <155614875+ynbdissanayake@users.noreply.github.com>
2026-03-30 16:54:21 +0530

    Database connection and schema validation.

--- patient-service/app/main.py
+++ patient-service/app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from pymongo import MongoClient
+from pydantic import BaseModel, Field
+import os
 
 load_dotenv()
 
@@ -8,3 +11,18 @@ app = FastAPI(
     version="1.0.0",
     root_path="/patients"
 )
+
+mongo_url = os.getenv("MONGO_URL")
+if not mongo_url:
+    raise ValueError("MONGO_URL is not set in .env")
+
+client = MongoClient(mongo_url)
+db = client["hospital_db"]
+patients_collection = db["patients"]
+counters_collection = db["counters"]
+
+
+class PatientCreate(BaseModel):
+    name: str = Field(..., min_length=2, max_length=100)
+    age: int = Field(..., ge=0, le=120)
+    contact: str = Field(..., min_length=7, max_length=20)
