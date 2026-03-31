42d9778e1b3dd86a2e8a9524b85327dad91da3d2
YasiruNB <155614875+ynbdissanayake@users.noreply.github.com>
2026-03-31 06:19:35 +0530

    Update main.py

--- patient-service/app/main.py
+++ patient-service/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from pymongo import MongoClient
+from pymongo import MongoClient, ReturnDocument
 from pydantic import BaseModel, Field
 import os
 
@@ -26,3 +26,13 @@ class PatientCreate(BaseModel):
     name: str = Field(..., min_length=2, max_length=100)
     age: int = Field(..., ge=0, le=120)
     contact: str = Field(..., min_length=7, max_length=20)
+
+
+def get_next_sequence(sequence_name: str) -> int:
+    counter = counters_collection.find_one_and_update(
+        {"_id": sequence_name},
+        {"$inc": {"sequence_value": 1}},
+        upsert=True,
+        return_document=ReturnDocument.AFTER
+    )
+    return counter["sequence_value"]
